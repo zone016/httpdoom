5e2cf94e9b845937b69d3c2b8a712213918bdde9
Jojo <jonas.uliana@passwd.com.br>
2020-12-20 05:17:35 -0300

    Added threads option

--- Records/Options.cs
+++ Records/Options.cs
@@ -1,9 +1,12 @@
-﻿namespace HttpDoom.Records
+﻿using System;
+
+namespace HttpDoom.Records
 {
     public class Options
     {
         public bool Debug { get; set; } = false;
         public int HttpTimeout { get; set; } = 5000;
+        public int Threads { get; set; } = Environment.ProcessorCount;
         public string OutputFile { get; set; }
         public int[] Ports { get; set; } = {80, 443, 8080, 8443};
         public string Proxy { get; set; }
